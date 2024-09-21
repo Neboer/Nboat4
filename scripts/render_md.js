2bc49165a21779fee6d77128654bb63779375b33
Neboer <rubinposter@gmail.com>
2024-09-21 17:10:09 +0800

    增加博客meta的详细显示功能，并针对移动端进行优化。同时，给博客标题也加上可以方便定位的id了。不过博客标题还是不能随着滚动而被选中。

--- scripts/render_md.js
+++ scripts/render_md.js
@@ -2,6 +2,11 @@ const { marked } = require('marked');
 const { gfmHeadingId } = require("marked-gfm-heading-id")
 
 marked.use(gfmHeadingId({}))
+
 hexo.extend.renderer.register("md", "html", function (data, options) {
     return marked.parse(data.text)
 }, true);
+
+hexo.extend.helper.register('get_title_html', function (blog_title) {
+    return marked.parse("# " + blog_title)
+})
