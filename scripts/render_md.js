4c318e5f3226b0ec823bd5991a9330dbba2285af
Neboer <rubinposter@gmail.com>
2024-11-24 17:33:50 +0800

    修好了Nboat4的一个bug——当某标题以数字开头的时候，它的ID无法被解析器识别，从而造成Bootstrap5的scrollspy无法正确检测用户滚动的位置

--- scripts/render_md.js
+++ scripts/render_md.js
@@ -1,7 +1,11 @@
 const { marked } = require('marked');
 const { gfmHeadingId } = require("marked-gfm-heading-id")
 
-marked.use(gfmHeadingId({}))
+const options = {
+	prefix: "nctitle-",
+};
+
+marked.use(gfmHeadingId(options))
 
 hexo.extend.renderer.register("md", "html", function (data, options) {
     return marked.parse(data.text)
